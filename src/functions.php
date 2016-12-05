f7ee1c29f42fad46963ecab764a8c9e02e7af868
SimVil <simon.vilmin@gmail.com>
2016-12-05 22:02:32 +0100

    adding comment to smiley function

--- src/functions.php
+++ src/functions.php
@@ -2,8 +2,8 @@
 <?php
 
 /* --------------------------------------------------------------------
- * FILE : functions.php 
- * 
+ * FILE : functions.php
+ *
  * Author : Amin, Simon
  * ------------------------------------------------------------------ */
 
@@ -11,12 +11,12 @@
 
 /* -------- DecodeFile ---------------------------------------------- */
 /* Input  : a file name
- * Output : an array decoded from the file 
- * 
+ * Output : an array decoded from the file
+ *
  * Proceed to decoding an array from a file in json format. The file
  * shall contain names and password of each users
  * ------------------------------------------------------------------ */
- 
+
 function DecodeFile($filename){
   $users = file_get_contents($filename, false, NULL);
   $arr = json_decode($users, true);
@@ -29,10 +29,10 @@ function DecodeFile($filename){
 /* -------- ExistUser ----------------------------------------------- */
 /* Input  : a login
  * Output : true whenever the user has been found, false otherwise.
- * 
+ *
  * Check whether a given user exist in the database.
  * ------------------------------------------------------------------ */
- 
+
 function ExistUser($log){
   $arr = DecodeFile('../db/users.txt');
   $found = false;
@@ -54,11 +54,11 @@ function ExistUser($log){
 /* -------- ValidUser ----------------------------------------------- */
 /* Input  : a login and a password
  * Output : true if data are valid, false otherwise.
- * 
+ *
  * Verify that log and pwd are joint in database, that is, the username
  * and the password given are valid.
  * ------------------------------------------------------------------ */
- 
+
 function ValidUser($log, $pwd){
   $arr = DecodeFile('../db/users.txt');
   $coded_pwd = hash("sha256", "$pwd");
@@ -70,9 +70,9 @@ function ValidUser($log, $pwd){
 
 
 /* -------- IsConnected --------------------------------------------- */
-/* Input  : a login  
+/* Input  : a login
  * Output : true if the user is connected, false if not.
- * 
+ *
  * Test if a user si connected or not.
  * ------------------------------------------------------------------ */
 
@@ -92,23 +92,23 @@ function IsConnected($log){
 
 
 /* -------- GetConnected -------------------------------------------- */
-/* Input  : a login 
+/* Input  : a login
  * Output : true if connection succeeded, false otherwise.
- * 
+ *
  * Get an user connected, if it is not already.
  * ------------------------------------------------------------------ */
 
 function GetConnected($log){
   $filename = fopen('../db/online.txt', 'a');
   $result = false;
-   
+
   if($filename && !IsConnected($log)){
     fwrite($filename, $log."\n");
     fclose($filename);
     $result = true;
-    
+
   }
-  
+
   return $result;
 
 }
@@ -116,9 +116,9 @@ function GetConnected($log){
 
 
 /* -------- EncodeUser ---------------------------------------------- */
-/* Input  : a login, a password 
+/* Input  : a login, a password
  * Output : true for success, false for fail.
- * 
+ *
  * Write the new user into the database file.
  * ------------------------------------------------------------------ */
 
@@ -130,12 +130,12 @@ function EncodeUser($log, $pwd){
   if(!$arr){
     $arr[] = array("login" => "$log", "pass" => "$coded_pwd");
     $result = true;
-    
+
   } else {
 	if(array_search($log, $arr) === false){
 		array_push($arr, array("login" => "$log", "pass" => "$coded_pwd"));
 		$result = true;
-		
+
 	}
 
   }
@@ -147,17 +147,20 @@ function EncodeUser($log, $pwd){
 }
 
 
-<?php
-//manage the emoticons
+/* -------- smileys ------------------------------------------------- */
+/* Input  : a text with written smileys
+ * Output : the text with replaced smileys
+ *
+ * Given a text, replace each occurence of a smiley with a real one.
+ * ------------------------------------------------------------------ */
+
 function smileys($text) {
-  $smileys = array(":)",";)","^^",":@",":'(",":p",":(");   
+  $smileys = array(":)",";)","^^",":@",":'(",":p",":(");
   $paths = array("./emoticons/smiley.png","./emoticons/wink.png","./emoticons/content.png","./emoticons/angry.gif","./emoticons/pleure.png","./emoticons/tongue.png","./emoticons/triste.png");
   for($i=0;$i<count($smileys);$i++) {
     $text = str_replace($smileys[$i],'<img  src='.$paths[$i].'>',$text);
     }
   return $text ;
 }
-?>
-
 
  ?>
